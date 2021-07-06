d7aaf1be14bf9268278fc6dd2c2c70d97389341a
Anna Freimuth <a.freimuth90@gmail.com>
2021-07-06 20:14:38 +0200

    added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule
