b59d3f21cfdb846f3bc99cf1117025cd9d83a7de
Anna Freimuth <a.freimuth90@gmail.com>
2021-07-24 19:52:43 +0200

    changed paths

--- src/app/service/statistics.service.ts
+++ src/app/service/statistics.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Statistic} from "../model/statistic";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatisticsService {
-  private getAllPath:string='http://localhost:8080/statistics';
+  private getAllPath:string=`${environment.backend}/statistics`;
   constructor(private http: HttpClient) { }
 
   getStatistics(): Observable<Statistic[]>{
--- src/app/service/url.service.ts
+++ src/app/service/url.service.ts
@@ -3,12 +3,13 @@ import {HttpClient} from "@angular/common/http";
 import {Model} from "../model/model";
 import {Observable} from "rxjs";
 import {ShortUrl} from "../model/short-url";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UrlService {
-  private addPath:string='http://localhost:8080/url';
+  private addPath:string=`${environment.backend}/url`;
   constructor(private http: HttpClient) { }
 
   addUrl(model : Model):Observable<ShortUrl>{
