3e8795fd9dee4c1f96b94f09102dc2816f485abb
Anna Freimuth <a.freimuth90@gmail.com>
2021-07-25 13:10:43 +0200

    added copy, open in a new tab link

--- src/app/pages/home/output-url-redirect/output-url-redirect.component.ts
+++ src/app/pages/home/output-url-redirect/output-url-redirect.component.ts
@@ -1,15 +1,21 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ShortUrl} from "../../../model/short-url";
+import {ClipboardService} from "ngx-clipboard";
 
 @Component({
   selector: 'app-output-url-redirect',
   templateUrl: './output-url-redirect.component.html',
-  styleUrls: ['./output-url-redirect.component.css']
+  styleUrls: ['./output-url-redirect.component.css'],
 })
 export class OutputUrlRedirectComponent implements OnInit {
 
   @Input()
-  shortUrl: ShortUrl | undefined;
+  shortUrl!: ShortUrl ;
+
+  isCopy = false;
+
+  constructor(private clipboardService: ClipboardService) {
+  }
 
   ngOnInit(): void {
   }
@@ -17,5 +23,23 @@ export class OutputUrlRedirectComponent implements OnInit {
   navigateByUrl(shortUrl: string) {
     window.location.href = shortUrl;
   }
+  copy() {
+    if (!this.shortUrl)
+      return;
 
+
+    this.clipboardService.copy(this.shortUrl.shortUrl);
+    this.isCopy = true;
+
+    setTimeout(() => {
+      this.isCopy = false;
+    }, 3000);
+  }
+
+  goToLink() {
+    if (!this.shortUrl)
+      return;
+
+    window.open(`${(this.shortUrl.shortUrl)}`, "_blank")
+  }
 }
